52ce6f917165d1d238ebe330babf66904e20960d
mopsfl <floda854@gmail.com>
2023-09-06 17:08:56 +0200

    updated

--- index.ts
+++ index.ts
@@ -7,9 +7,11 @@ import * as self from "./index"
 jQuery(() => {
     M.AutoInit()
 
-    const sidebar_buttons = document.querySelectorAll(".sidebar-btn")
+    const sidebar_buttons = document.querySelectorAll(".function-btn")
     sidebar_buttons.forEach(btn => {
         $(btn).on("click", async (e) => {
+            if (self.default.block_requests) return
+            self.default.block_requests = true
             Editor.ToggleLoading("Processing")
             const func = e.target.attributes.getNamedItem("data-function"),
                 _request = await Request.new(func, Editor.GetValue(), self.default.options)
@@ -18,7 +20,8 @@ jQuery(() => {
                 let _response_body = ""
                 if (_request.ok) _response_body = await _request.text()
                 Editor.SetValue(_response_body)
-                Editor.ToggleLoading("Processing")
+                Editor.ToggleLoading("")
+                self.default.block_requests = false
             }
         })
     })
@@ -29,6 +32,7 @@ jQuery(() => {
 })
 
 export default {
+    block_requests: false,
     options: {
         api_url: location.hostname == "localhost" ? "http://localhost:6969/api/goofyuglifier/" : "https://mopsflgithubio.mopsfl.repl.co/api/goofyuglifier/"
     }
