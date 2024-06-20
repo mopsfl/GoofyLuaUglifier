c58fa7dceae4e35ae69d94cceedb4544a1c4bff5
mopsfl <floda854@gmail.com>
2024-06-20 02:00:35 +0200

    updated docs

--- Docs.ts
+++ Docs.ts
@@ -22,4 +22,25 @@ jQuery(async () => {
             _collapsibleIcon && _collapsibleIcon.classList.remove("collopen")
         }
     })
+
+    document.querySelectorAll(".docs-endpoint").forEach(_el => {
+        const _copybtn = _el.querySelector(".docs-endpoint-copy")
+        if (_copybtn) {
+            _copybtn.addEventListener("click", (e) => {
+                const _content = _el.querySelector(".docs-endpoint-path")?.attributes["data-copycontent"]?.value
+                if (_content) {
+                    navigator.clipboard.writeText(_content).then(() => {
+                        _copybtn.textContent = "COPIED!"
+                        setTimeout(() => {
+                            _copybtn.textContent = "COPY"
+                        }, 1000);
+                    }).catch(console.error)
+                }
+            })
+
+            _el.addEventListener("mouseenter", () => _copybtn.classList.remove("copyhide"))
+            _el.addEventListener("mouseleave", () => _copybtn.classList.add("copyhide"))
+        }
+    })
+
 })
